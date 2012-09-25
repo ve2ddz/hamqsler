c6eb7a96e7c70c33e18d73a1bf456285bcb625d3
Jim Orcheson <jimorcheson@gmail.com>
2012-09-25 07:50:23 -0400

    Added splash screen. Modified Application to show the splash screen. When splash screen is closed, main window is shown.

--- hamqsler/App.xaml.cs
+++ hamqsler/App.xaml.cs
@@ -11,5 +11,22 @@ namespace hamqsler
 	/// </summary>
 	public partial class App : Application
 	{
+		
+		void Application_Startup(object sender, StartupEventArgs e)
+		{
+			// create and show SplashPage
+			SplashPage splash = new SplashPage();
+			splash.ShowDialog();
+		}
+		
+		/// <summary>
+		/// Create and show the program's main window
+		/// This should be called only once, from the SplashPage when it is closing
+		/// </summary>
+		internal void ShowMainWindow()
+		{
+			MainWindow win = new MainWindow();
+			win.Show();
+		}
 	}
 }
---
+++ hamqsler/SplashPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2012 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// Interaction logic for SplashPage.xaml
+	/// SplashPage is displayed when App is started.
+	/// It performs many initialization tasks, such as loading and
+	/// checking the UserPreferences,
+	/// and checking if a new version of the program is available.
+	/// </summary>
+	public partial class SplashPage : Window
+	{
+		public SplashPage()
+		{
+			InitializeComponent();
+		}
+		
+		/// <summary>
+		/// Called when SplashPage is closing to display MainWindow
+		/// </summary>
+		/// <param name="sender">not used</param>
+		/// <param name="e">not used</param>
+		void Window_Closing(object sender, EventArgs e)
+		{
+			App app = (App)Application.Current;
+			app.ShowMainWindow();
+		}
+		
+		/// <summary>
+		/// Perform all startup functions such as:
+		/// Load or create UserPreferences, and
+		/// check if new version of program is available.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			// TODO: Implement SplashPage.Window_Loaded
+		}
+	}
+}
