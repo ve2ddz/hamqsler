efcfd3c4fe74e6005465d5f8b78f8f8207ffbbb5
Jim Orcheson <jimorcheson@gmail.com>
2012-10-26 08:36:27 -0400

    Added converters for Sent, Rcvd, and SentVia QsoWithInclude properties.

---
+++ hamqsler/RcvdConverter.cs
@@ -0,0 +1,70 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2012 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// Converter for Rcvd property
+	/// </summary>
+	[ValueConversion(typeof(string), typeof(string))]
+	public class RcvdConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter,
+		                      CultureInfo culture)
+		{
+			string rcvdValue = (string)value;
+			string retValue = string.Empty;
+			switch(rcvdValue)
+			{
+				case "Y":
+					retValue = "Yes";
+					break;
+				case "N":
+					retValue = "No";
+					break;
+				case "R":
+					retValue = "Requested";
+					break;
+				case "V":
+					retValue = "Verified";
+					break;
+				case "I":
+					retValue = "Ignore";
+					break;
+				case "":
+					retValue = string.Empty;
+					break;
+				default:
+					retValue = "***";
+					break;
+			}
+			return retValue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter,
+		                      CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ hamqsler/SentViaConverter.cs
@@ -0,0 +1,67 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2012 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// Converter for SentVia property
+	/// </summary>
+	[ValueConversion(typeof(string), typeof(string))]
+	public class SentViaConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter,
+		                      CultureInfo culture)
+		{
+			string sentViaValue = (string)value;
+			string retValue = string.Empty;
+			switch(sentViaValue)
+			{
+				case "B":
+					retValue = "Bureau";
+					break;
+				case "D":
+					retValue = "Direct";
+					break;
+				case "E":
+					retValue = "Electronic";
+					break;
+				case "M":
+					retValue = "Manager";
+					break;
+				case "":
+					retValue = string.Empty;
+					break;
+				default:
+					retValue = "***";
+					break;
+			}
+			return retValue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter,
+		                      CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
