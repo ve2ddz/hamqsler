b38eb8495dd5818a6390c45bdcb373744677fb6d
Jim Orcheson <jimorcheson@gmail.com>
2012-11-08 08:27:40 -0500

    Removed debug code from CardImageBase.OnRender.

--- hamqsler/CardImageBase.cs
+++ hamqsler/CardImageBase.cs
@@ -243,11 +243,6 @@ namespace hamqsler
 		protected override void OnRender(DrawingContext dc)
 		{
 			Rect rect = DisplayRectangle;
-			if(this.GetType() == typeof(SecondaryImage))
-		    {
-				System.Diagnostics.Debug.WriteLine("{0}, {1} : {2}, {3}", 
-				                                   rect.X, rect.Y, rect.Width, rect.Height);
-		    }
 			Brush brush = Brushes.Transparent;
 			if(ImageFileName == string.Empty || ImageFileName == null)
 			{
