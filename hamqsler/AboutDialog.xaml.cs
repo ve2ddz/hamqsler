2738237033b79f7fe6f897e24a86fb050b44a579
Jim Orcheson <jimorcheson@gmail.com>
2013-03-23 10:00:08 -0400

    Replaced generic header comments with solution specific comments in AboutDialog.xaml.cs.

--- hamqsler/AboutDialog.xaml.cs
+++ hamqsler/AboutDialog.xaml.cs
@@ -1,10 +1,21 @@
 ﻿/*
- * Created by SharpDevelop.
- * User: Jim
- * Date: 06/03/2013
- * Time: 10:53 AM
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
  * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 using System;
 using System.Collections.Generic;
