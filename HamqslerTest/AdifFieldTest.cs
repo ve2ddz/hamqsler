6f72a0f1dc53263df23dd51674ea6f0e23401e31
Jim Orcheson <jimorcheson@gmail.com>
2013-08-16 12:16:26 -0400

    New files that should have been added with previous commit.

---
+++ HamqslerTest/AdifFieldTest.cs
@@ -0,0 +1,72 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	/// <summary>
+	/// Description of AdifFieldTest.
+	/// </summary>
+	[TestFixture]
+	public class AdifFieldTest
+	{
+		// test Name accessor
+		[Test]
+		public void TestGetName()
+		{
+			AdifField field = new AdifField("VA3HJ");
+			Assert.AreEqual("AdifField", field.Name);
+		}
+		
+		// test Value accessor
+		[Test]
+		public void TestGetValue()
+		{
+			AdifField field = new AdifField("VA3HJ");
+			Assert.AreEqual("VA3HJ", field.Value);
+		}
+		
+		// test ToAdifField
+		[Test]
+		public void TestToAdifString()
+		{
+			AdifField field = new AdifField("VA3HJ");
+			Assert.AreEqual("<AdifField:5>VA3HJ", field.ToAdifString());
+		}
+		
+		// test IsValid
+		[Test]
+		public void TestIsValid()
+		{
+			AdifField field = new AdifField("VA3HJ");
+			Assert.IsTrue(field.IsValid());
+		}
+		
+		// test IsValid returns false
+		[Test]
+		public void TestIsValidFalse()
+		{
+			AdifField field = new AdifField(null);
+			Assert.IsFalse(field.IsValid());
+		}
+	}
+}
---
+++ hamqsler/AdifField.cs
@@ -0,0 +1,69 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// Base class for all Adif fields
+	/// </summary>
+	public class AdifField
+	{
+		public string Name
+		{
+			get {return this.GetType().ToString().Substring("hamqsler.".Length);}
+		}
+		
+		private string eltValue = string.Empty;
+		public string Value
+		{
+			get {return eltValue;}
+			set {eltValue = value;}
+		}
+		
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="value">field value</param>
+		public AdifField(string value)
+		{
+			Value = value;
+		}
+		
+		/// <summary>
+		/// Create ADIF field string from contents of this element
+		/// </summary>
+		/// <returns>ADIF field string</returns>
+		public virtual string ToAdifString()
+		{
+			return "<" + Name + ":" + Value.Length + ">" + Value;
+		}
+		
+		/// <summary>
+		/// Validate the field
+		/// </summary>
+		/// <returns>true if Value is not null</returns>
+		public virtual bool IsValid()
+		{
+			return Value != null;
+		}
+		
+	}
+}
