a5e1353b9a649d3b9fcdb45ac0910cf8fecbe4e7
Jim Orcheson <jimorcheson@gmail.com>
2013-08-18 08:25:38 -0400

    Added classes NumberField, Age, AntAz, AntEl, AIndex, and NumberFieldTest containing NUnit tests for NumberField.

---
+++ HamqslerTest/NumberFieldTest.cs
@@ -0,0 +1,115 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	/// <summary>
+	/// Test class for NumberField class
+	/// </summary>
+	[TestFixture]
+	public class NumberFieldTest
+	{
+		// test that IsValid returns true for integer value
+		[Test]
+		public void TestIsValid()
+		{
+			NumberField nf = new NumberField("94");
+			Assert.IsTrue(nf.IsValid());
+		}
+		
+		// test IsValid returns true for decimal value 
+		[Test]
+		public void TestIsValid1()
+		{
+			NumberField nf = new NumberField("94.21345");
+			Assert.IsTrue(nf.IsValid());
+		}
+		
+		// test IsValid returns true for decimal value starting with decimal separator
+		// Note: Adif standard states that decimal separator is "."
+		[Test]
+		public void TestIsValid2()
+		{
+			NumberField nf = new NumberField(".27");
+			Assert.IsTrue(nf.IsValid());
+		}
+		
+		// test IsValid returns true for decimal value ending with decimal separator
+		[Test]
+		public void TestIsValid3()
+		{
+			NumberField nf = new NumberField("27.");
+			Assert.IsTrue(nf.IsValid());
+		}
+		
+		// test IsValid return true for empty string value
+		[Test]
+		public void TestIsValid4()
+		{
+			NumberField nf = new NumberField("");
+			Assert.IsTrue(nf.IsValid());
+		}
+		
+		// test IsValid returns true for negative number
+		[Test]
+		public void TestIsValid5()
+		{
+			NumberField nf = new NumberField("-5");
+			Assert.IsTrue(nf.IsValid());
+		}
+		
+		// test IsValid returns true for negative number with decimal sep
+		[Test]
+		public void TestIsValid6()
+		{
+			NumberField nf = new NumberField("-2.3");
+			Assert.IsTrue(nf.IsValid());
+		}
+		
+		/// <summary>
+		/// test that IsValid returns false for a non number value.
+		/// </summary>
+		[Test]
+		public void TestIsNotValid()
+		{
+			NumberField nf = new NumberField("fred");
+			Assert.IsFalse(nf.IsValid());
+		}
+		
+		// test that IsValid returns false for decimal separator only
+		[Test]
+		public void TestIsNotValid1()
+		{
+			NumberField nf = new NumberField(".");
+			Assert.IsFalse(nf.IsValid());
+		}
+		
+		// test that IsValid returns false for 2 decimal separators
+		[Test]
+		public void TestIsNotValid2()
+		{
+			NumberField nf = new NumberField("4.2.5");
+			Assert.IsFalse(nf.IsValid());
+		}
+	}
+}
---
+++ hamqsler/NumberField.cs
@@ -0,0 +1,51 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Text.RegularExpressions;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// Base class for all number based ADIF Fields
+	/// </summary>
+	public class NumberField : AdifField
+	{
+		/// <summary>
+		/// constructor
+		/// </summary>
+		/// <param name="value">Value for the field</param>
+		public NumberField(string value) : base(value)
+		{
+		}
+		
+		/// <summary>
+		/// Validates the value as a number
+		/// </summary>
+		/// <returns>true if value is empty or a number</returns>
+		public override bool IsValid()
+		{
+			if(string.Equals(string.Empty, Value))
+		    {
+		   		return true;
+		    }
+			return Regex.IsMatch(Value, @"^-{0,1}([\d]+|[\d]+\.[\d]*|\.[\d]+)$");
+		}
+	}
+}
