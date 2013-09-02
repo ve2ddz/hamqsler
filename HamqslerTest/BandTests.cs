929e1f4ff228c2bdaa0e9504888644ad94e8dd44
Jim Orcheson <jimorcheson@gmail.com>
2013-09-02 06:25:21 -0400

    Added Band.cs and BandTests.cs which should have been included in the immediately previous commit.

---
+++ HamqslerTest/BandTests.cs
@@ -0,0 +1,131 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for Band class
+	[TestFixture]
+	public class BandTests
+	{
+		// test Validate with valid value
+		[Test]
+		public void TestValidate2190m()
+		{
+		    // get the hamqsler assembly
+			Assembly assembly = Assembly.GetAssembly((new AdifField(string.Empty)).GetType());
+            // get a stream for the AdifEnumerations.xml file
+            // TODO: This is currently an embedded resource in the assembly, but needs to be moved to AppData
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+             // load in the xml file
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Band band = new Band("2190m", aEnums);
+			string error = string.Empty;
+			Assert.IsTrue(band.Validate(out error));
+		}
+
+		// test Validate with valid value
+		[Test]
+		public void TestValidate1mm()
+		{
+		    // get the hamqsler assembly
+			Assembly assembly = Assembly.GetAssembly((new AdifField(string.Empty)).GetType());
+            // get a stream for the AdifEnumerations.xml file
+            // TODO: This is currently an embedded resource in the assembly, but needs to be moved to AppData
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+             // load in the xml file
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Band band = new Band("1mm", aEnums);
+			string error = string.Empty;
+			Assert.IsTrue(band.Validate(out error));
+		}
+
+		// test Validate with invalid value
+		[Test]
+		public void TestValidateBadValue()
+		{
+		    // get the hamqsler assembly
+			Assembly assembly = Assembly.GetAssembly((new AdifField(string.Empty)).GetType());
+            // get a stream for the AdifEnumerations.xml file
+            // TODO: This is currently an embedded resource in the assembly, but needs to be moved to AppData
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+             // load in the xml file
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Band band = new Band("23mm", aEnums);
+			string error = string.Empty;
+			Assert.IsFalse(band.Validate(out error));
+			Assert.AreEqual("This QSO Field is of type enumeration. The value '23mm' was not found in enumeration", 
+			                error);
+		}
+
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+		    // get the hamqsler assembly
+			Assembly assembly = Assembly.GetAssembly((new AdifField(string.Empty)).GetType());
+            // get a stream for the AdifEnumerations.xml file
+            // TODO: This is currently an embedded resource in the assembly, but needs to be moved to AppData
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+             // load in the xml file
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Band band = new Band("23cm", aEnums);
+			string error = string.Empty;
+			Assert.AreEqual("<Band:4>23cm", band.ToAdifString());
+		}
+		
+		// test IsWithinBand for frequency within the band
+		[Test]
+		public void TestIsWithinBandValidFreq()
+		{
+		    // get the hamqsler assembly
+			Assembly assembly = Assembly.GetAssembly((new AdifField(string.Empty)).GetType());
+            // get a stream for the AdifEnumerations.xml file
+            // TODO: This is currently an embedded resource in the assembly, but needs to be moved to AppData
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+             // load in the xml file
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Band band = new Band("40m", aEnums);
+			string error = string.Empty;
+			Assert.IsTrue(band.IsWithinBand("7.102"));
+		}
+		
+		// test IsWithinBand for frequency outside of the band
+		[Test]
+		public void TestIsWithinBandBadFreq()
+		{
+		    // get the hamqsler assembly
+			Assembly assembly = Assembly.GetAssembly((new AdifField(string.Empty)).GetType());
+            // get a stream for the AdifEnumerations.xml file
+            // TODO: This is currently an embedded resource in the assembly, but needs to be moved to AppData
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+             // load in the xml file
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Band band = new Band("40m", aEnums);
+			string error = string.Empty;
+			Assert.IsFalse(band.IsWithinBand("14.302"));
+		}
+
+	}
+}
---
+++ hamqsler/Band.cs
@@ -0,0 +1,58 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Globalization;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// Band class - specifies the logging station's transmitting band
+	/// </summary>
+	public class Band : EnumerationValue
+	{
+		/// <summary>
+		/// constructor
+		/// </summary>
+		/// <param name="band">Band value</param>
+		/// <param name="aEnums">AdifEnumerations object containing the Band enumeration</param>
+		public Band(string band, AdifEnumerations aEnums) : base(band, "Band", aEnums)
+		{
+		}
+		
+		/// <summary>
+		/// Checks if the input frequency is within this band
+		/// </summary>
+		/// <param name="freq">Frequency to check</param>
+		/// <returns>true if within the band, false otherwise</returns>
+		public bool IsWithinBand(string freq)
+		{
+			string lLimit = string.Empty;
+			string uLimit = string.Empty;
+			float f = float.Parse(freq, CultureInfo.InvariantCulture);
+			if(aEnums.GetBandLimits(Value, out lLimit, out uLimit))
+			{
+				float lFreq = float.Parse(lLimit, CultureInfo.InvariantCulture);
+				float uFreq = float.Parse(uLimit, CultureInfo.InvariantCulture);
+				return f >= lFreq && f <= uFreq;
+			}
+			return false;
+		}
+	}
+}
