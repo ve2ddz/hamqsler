f5b9584a94ec9f1d77b2cc73a81cd573a1950171
Jim Orcheson <jimorcheson@gmail.com>
2013-09-03 15:13:05 -0400

    Added Call class and CallTests class.

---
+++ HamqslerTest/CallTests.cs
@@ -0,0 +1,291 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for Call class
+	[TestFixture]
+	public class CallTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			Call call = new Call("VA3HJ");
+			Assert.AreEqual("<Call:5>VA3HJ", call.ToAdifString());
+		}
+		
+		// test ToAdifString for W4/VA3HJ/M
+		[Test]
+		public void TestToAdifStringPreSuf()
+		{
+			Call call = new Call("W4/VA3HJ/M");
+			Assert.AreEqual("<Call:10>W4/VA3HJ/M", call.ToAdifString());
+		}
+		
+		// test Validate with simple callsign
+		[Test]
+		public void TestValidateVA3HJ()
+		{
+			Call call = new Call("VA3HJ");
+			string error = string.Empty;
+			Assert.IsTrue(call.Validate(out error));
+		}
+		
+		// test Validate with simple callsign
+		[Test]
+		public void TestValidate6Y5BF()
+		{
+			Call call = new Call("6Y5BF");
+			string error = string.Empty;
+			Assert.IsTrue(call.Validate(out error));
+		}
+		
+		// test Validate with invalid callsign
+		[Test]
+		public void TestValidateVAHJ()
+		{
+			Call call = new Call("VAHJ");
+			string error = string.Empty;
+			Assert.IsFalse(call.Validate(out error));
+			Assert.AreEqual("Callsign 'VAHJ' is invalid.", error);
+		}
+		
+		// test Validate with another invalid callsign
+		[Test]
+		public void TestValidate9A()
+		{
+			Call call = new Call("9A");
+			string error = string.Empty;
+			Assert.IsFalse(call.Validate(out error));
+			Assert.AreEqual("Callsign '9A' is invalid.", error);
+		}
+		
+		// test Validate with prefix
+		[Test]
+		public void TestValidatePrefix()
+		{
+			Call call = new Call("GB2/VA3HJ");
+			string error = string.Empty;
+			Assert.IsTrue(call.Validate(out error));
+		}
+		
+		// test Validate with empty callsign
+		[Test]
+		public void TestValidateEmpty()
+		{
+			Call call = new Call(string.Empty);
+			string error = string.Empty;
+			Assert.IsFalse(call.Validate(out error));
+			Assert.AreEqual("Callsign '' is invalid.", error);
+		}
+		
+		// test Validate with empty callsign
+		[Test]
+		public void TestValidateNull()
+		{
+			Call call = new Call(null);
+			string error = string.Empty;
+			Assert.IsFalse(call.Validate(out error));
+			Assert.AreEqual("Null callsign is invalid.", error);
+		}
+		
+		// test Validate for OEH20
+		[Test]
+		public void TestValidateOEH20()
+		{
+			Call call = new Call("OEH20");
+			string error = string.Empty;
+			Assert.IsTrue(call.Validate(out error));
+			Assert.AreEqual(string.Empty, error);
+		}
+		
+		// test Validate for ZW1CCOM54
+		[Test]
+		public void TestValidateZW1CCOM54()
+		{
+			Call call = new Call("ZW1CCOM54");
+			string error = string.Empty;
+			Assert.IsTrue(call.Validate(out error));
+			Assert.AreEqual(string.Empty, error);
+		}
+		
+		// test GetCall with no prefix or suffix
+		[Test]
+		public void TestGetCallNone()
+		{
+			Call call = new Call("VA3HJ");
+			Assert.AreEqual("VA3HJ", call.GetCall());
+		}
+		
+		// test GetCall with invalid callsign
+		[Test]
+		public void TestGetCallInvalidCall()
+		{
+			Call call = new Call("VAHJ");
+			Assert.AreEqual(null, call.GetCall());
+		}
+		
+		// test GetCall with prefix
+		[Test]
+		public void TestGetCallPrefix()
+		{
+			Call call = new Call("9A5/VA3HJ");
+			Assert.AreEqual("VA3HJ", call.GetCall());
+		}
+		
+		// test GetCall with suffix
+		[Test]
+		public void TestGetCallSuffix()
+		{
+			Call call = new Call("VA3HJ/P");
+			Assert.AreEqual("VA3HJ", call.GetCall());
+		}
+		
+		// test GetCall with 3D2R prefix
+		[Test]
+		public void TestGetCall3D2RPrefix()
+		{
+			Call call = new Call("3D2R/VA3HJ");
+			Assert.AreEqual("VA3HJ", call.GetCall());
+		}			
+		
+		// test GetCall with VP6D prefix
+		[Test]
+		public void TestGetCallVP6DPrefix()
+		{
+			Call call = new Call("VP6D/VA3HJ");
+			Assert.AreEqual("VA3HJ", call.GetCall());
+		}			
+		
+		// test GetCall with VP6D prefix
+		[Test]
+		public void TestGetCallR1FJPrefix()
+		{
+			Call call = new Call("R1FJ/UA0BMD");
+			Assert.AreEqual("UA0BMD", call.GetCall());
+		}
+		
+		// test GetCall with VP2E
+		[Test]
+		public void TestGetCallVP2E()
+		{
+			Call call = new Call("VP2E");
+			Assert.AreEqual("VP2E", call.GetCall());
+		}
+		
+		// test GetCall with VP2E prefix
+		[Test]
+		public void TestGetCallVP2EPrefix()
+		{
+			Call call = new Call("VP2E/VA3HJ");
+			Assert.AreEqual("VA3HJ", call.GetCall());
+		}
+		
+		// test GetCall with XM31812
+		[Test]
+		public void TestGetCallXM31812()
+		{
+			Call call = new Call("XM31812");
+			Assert.AreEqual("XM31812", call.GetCall());
+		}
+		
+		// test IsValid with null call
+		[Test]
+		public void TestIsValidNullCall()
+		{
+			Assert.IsFalse(Call.IsValid(null));
+		}
+		
+		// test IsValid with empty call
+		[Test]
+		public void TestIsValidEmptyCall()
+		{
+			Assert.IsFalse(Call.IsValid(string.Empty));
+		}
+		
+		// test IsValid with simple call
+		[Test]
+		public void TestIsValidSimpleCall()
+		{
+			Assert.IsTrue(Call.IsValid("VA3HJ"));
+		}
+		
+		// test IsValid with invalid call
+		[Test]
+		public void TestIsValidBadCall()
+		{
+			Assert.IsFalse(Call.IsValid("VAHJ"));
+		}
+		
+		// test IsValid with prefix
+		[Test]
+		public void TestIsValidWithPrefix()
+		{
+			Assert.IsFalse(Call.IsValid("GB2/VA3HJ"));
+		}
+		
+		// test IsValid with suffix
+		[Test]
+		public void TestIsValidWithSuffix()
+		{
+			Assert.IsFalse(Call.IsValid("VA3HJ/M"));
+		}
+		
+		// test IsValid for BM100
+		[Test]
+		public void TestIsValidBM100()
+		{
+			Assert.IsTrue(Call.IsValid("BM100"));
+		}
+		
+		// test IsValid for BW100
+		[Test]
+		public void TestIsValidBW100()
+		{
+			Assert.IsTrue(Call.IsValid("BW100"));
+		}
+		
+		// test IsValid for LM9L40Y
+		[Test]
+		public void TestIsValidLM9L40Y()
+		{
+			Assert.IsTrue(Call.IsValid("LM9L40Y"));
+		}
+		
+		// test IsValid for SX1912
+		[Test]
+		public void TestIsValidSX1912()
+		{
+			Assert.IsTrue(Call.IsValid("SX1912"));
+		}
+		
+		// test IsValid for 8J2KSG7X
+		[Test]
+		public void TestIsValid8J2KSG7X()
+		{
+			Assert.IsTrue(Call.IsValid("8J2KSG7X"));
+		}
+		
+	}
+}
---
+++ hamqsler/Call.cs
@@ -0,0 +1,207 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// Call class - reperesents ADIF Call field
+	/// </summary>
+	public class Call : StringField
+	{
+        // List for holding the regular expressions for ARRL prefixes that might be 
+        // misinterpreted as callsigns
+        private static List<Regex> arrlPrefixes = new List<Regex>();
+		
+		// the following array contains prefixes that may be misinterpreted as callsigns,
+		// so special processing will be required.
+		private static string[] prefixes = {
+			"3D2[CR]",
+			"BV9P",
+			"CE0[AXZ]",
+			"E51[NS]",
+			"FO0[AM]",
+			"FT[0-9][WX]",
+			"HK0[AM]",
+			"JD1[MO]",
+			"KH[57]K",
+			"PY0[FT]",
+			"R1FJ",
+			"R1MV",
+			"VK0[HM]",
+			"VK9[CLMNWX]",
+			"VP2[MV]",
+			"VP6[DP]",
+			"VP8[FGHOS]"
+		};
+		
+		// the following array contains valid, non-standard callsigns
+		private static string[] nsCalls = {
+			"BM100",
+			"BN100",
+			"BO100",
+			"BP100",
+			"BQ100",
+			"BU100",
+			"BV100",
+			"BW100",
+			"BX100",
+			"LM9L40Y",
+			"OEH20",
+			"SX1912",
+			"VI6ARG30",
+			"XM31812",
+			"TE1856",
+			"8J2KSG7X",
+			"ZW1CCOM54"			
+		};
+
+		/// <summary>
+		/// Static constructor
+		/// </summary>
+		static Call()
+		{
+			foreach(string prefix in prefixes)
+			{
+				arrlPrefixes.Add(new Regex("^" + prefix + "$"));
+			}
+
+		}
+		
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="call">callsign</param>
+		public Call(string call) : base(call)
+		{
+		}
+		
+		/// <summary>
+		/// Validate the callsign
+		/// </summary>
+		/// <param name="err">Error message if callsign is not valid</param>
+		/// <returns>True if callsign is valid, false otherwise</returns>
+		public override bool Validate(out string err)
+		{
+			err = string.Empty;
+			if(Value == null)
+			{
+				err = "Null callsign is invalid.";
+				return false;
+			}
+			string call = GetCall();
+			if(call != null)
+			{
+				if(IsValid(call))
+				{
+					return true;
+				}
+			}
+			err = string.Format("Callsign '{0}' is invalid.", Value);
+			return false;
+		}
+		
+		/// <summary>
+		/// Gets calsign from possible prefix/call/suffixes
+		/// </summary>
+		/// <returns>Callsign if valid, or null if no callsign found</returns>
+		public string GetCall()
+		{
+			List<string> calls = new List<string>();
+			string[] parts = Value.Split('/');
+			foreach(string c in parts)
+			{
+				string cc = c;
+				if(Call.IsValid(c))
+				{
+                    foreach (Regex reg in arrlPrefixes)
+                    {
+                        // if prefix, then ignore it
+                        if (reg.IsMatch(c))
+                        {
+                            cc = null;
+                            break;
+                        }
+                    }
+                    // not a prefix, so add to list
+                    if (cc != null)
+                    {
+                        calls.Add(cc);
+                    }
+				}
+			}
+			if(calls.Count < 1)
+			{
+				return null;
+			}
+			else if(calls.Count == 1)
+			{
+				return calls[0];
+			}
+            else
+            {
+                // if more than one valid call
+                foreach (string c in calls)
+                {
+                    // VP2E is special case where it is both a valid prefix and a valid callsign
+                    if (c.Equals("VP2E"))
+                    {
+                        calls.Remove("VP2E");
+                        break;
+                    }
+                }
+                // now assume that the first call is the valid one.
+                // this will be the case except in strange circumstances.
+                return calls[0];
+            }
+		}
+		
+		/// <summary>
+		/// Determine if input callsign is valid. Callsign cannot contain prefix or suffix
+		/// </summary>
+		/// <param name="callsign">Callsign to check for validity</param>
+		/// <returns>True if callsign is valid, false otherwise</returns>
+		public static bool IsValid(string callsign)
+		{
+			if(callsign == null || callsign.Equals(string.Empty))
+			{
+				return false;
+			}
+			else if(Regex.IsMatch(callsign.ToUpper(), "^[0-9]{0,1}[A-Z]{1,2}[0-9]{1,4}[A-Z]+$"))
+			{
+				return true;
+			}
+			else 	
+			{
+				// special callsigns
+				foreach(string nsCall in nsCalls)
+				{
+					if(callsign == nsCall)
+					{
+						return true;
+					}
+				}
+				return false;
+			}
+		}
+	}
+}
