9e6ff70f465e40691395ca8576d4194c0188890b
Jim Orcheson <jimorcheson@gmail.com>
2013-09-04 06:59:48 -0400

    Added DateField and DateFieldTests classes.

---
+++ HamqslerTest/DateFieldTests.cs
@@ -0,0 +1,150 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for DateField class
+	[TestFixture]
+	public class DateFieldTests
+	{
+		// test ToAdifField
+		[Test]
+		public void TestToAdifField()
+		{
+			DateField date = new DateField("19960412");
+			Assert.AreEqual("<DateField:8>19960412", date.ToAdifString());
+		}
+		
+		// test Validate with valid date
+		[Test]
+		public void TestValidateValidDate()
+		{
+			DateField date = new DateField("19960412");
+			string err = string.Empty;
+			Assert.IsTrue(date.Validate(out err));
+		}
+		
+		// test Validate with date that is too short
+		[Test]
+		public void TestValidateDateTooShort()
+		{
+			DateField date = new DateField("1996412");
+			string err = string.Empty;
+			Assert.IsFalse(date.Validate(out err));
+			Assert.AreEqual("Date must be exactly 8 characters long", err);
+		}
+		
+		// test Validate with date that is too long
+		[Test]
+		public void TestValidateDateTooLong()
+		{
+			DateField date = new DateField("199604123");
+			string err = string.Empty;
+			Assert.IsFalse(date.Validate(out err));
+			Assert.AreEqual("Date must be exactly 8 characters long", err);
+		}
+		
+		// test Validate with date containing non-numeric characters
+		[Test]
+		public void TestValidateNonNumeric()
+		{
+			DateField date = new DateField("1996MR12");
+			string err = string.Empty;
+			Assert.IsFalse(date.Validate(out err));
+			Assert.AreEqual("Date must contain number characters only", err);
+		}
+		
+		// test Validate with date before 1930
+		[Test]
+		public void TestValidate1929()
+		{
+			DateField date = new DateField("19291231");
+			string err = string.Empty;
+			Assert.IsFalse(date.Validate(out err));
+			Assert.AreEqual("Date must be 19300101 or later", err);
+		}
+		
+		// test Validate with date later than now
+		[Test]
+		public void TestValidateLaterThanNow()
+		{
+			DateTime now = DateTime.UtcNow;
+			DateTime future = now.AddMonths(2);
+			string strNow = string.Format("{0:yyyyMMdd}", future);
+			DateField date = new DateField(strNow);
+			string err = string.Empty;
+			Assert.IsFalse(date.Validate(out err));
+			Assert.AreEqual("Date must not be later than today", err);
+		}
+		
+		// test Validate with invalid month (=00)
+		[Test]
+		public void TestValidateMonth00()
+		{
+			DateField date = new DateField("20130012");
+			string err = string.Empty;
+			Assert.IsFalse(date.Validate(out err));
+			Assert.AreEqual("Invalid month in date: must be between 01 and 12", err);
+		}
+		
+		// test Validate with invalid month (=13)
+		[Test]
+		public void TestValidateMonth13()
+		{
+			DateField date = new DateField("20121312");
+			string err = string.Empty;
+			Assert.IsFalse(date.Validate(out err));
+			Assert.AreEqual("Invalid month in date: must be between 01 and 12", err);
+		}
+		
+		// test Validate with invalid day (=00)
+		[Test]
+		public void TestValidateDay00()
+		{
+			DateField date = new DateField("20120400");
+			string err = string.Empty;
+			Assert.IsFalse(date.Validate(out err));
+			Assert.AreEqual("Invalid day in date: must be between 01 and end of month", err);
+		}
+		
+		// test Validate with invalid day (greater than last day of month)
+		[Test]
+		public void TestValidateDayAfterEndOfMonth()
+		{
+			DateField date = new DateField("20120431");
+			string err = string.Empty;
+			Assert.IsFalse(date.Validate(out err));
+			Assert.AreEqual("Invalid day in date: must be between 01 and end of month", err);
+		}
+		
+		// test Validate with Feb 29 of leap year
+		[Test]
+		public void TestValidateDayFebLeapYear()
+		{
+			DateField date = new DateField("20000229");
+			string err = string.Empty;
+			Assert.IsTrue(date.Validate(out err));
+		}
+		
+	}
+}
---
+++ hamqsler/DateField.cs
@@ -0,0 +1,85 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Text.RegularExpressions;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// DateField class - base class for classes that are of type Date
+	/// </summary>
+	public class DateField : StringField
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="date">Date in format YYYYMMDD</param>
+		public DateField(string date) : base(date)
+		{
+		}
+		
+		/// <summary>
+		/// Validate the value of the date field (between 19300101 and today)
+		/// </summary>
+		/// <param name="err">Error message to return if date not valid</param>
+		/// <returns>True if date is valid, false if not valid</returns>
+		public override bool Validate(out string err)
+		{
+			err = string.Empty;
+			if(Value.Length != 8)
+			{
+				err = "Date must be exactly 8 characters long";
+				return false;
+			}
+			if(!Regex.IsMatch(Value, "^[0-9]+$"))
+			{
+				err = "Date must contain number characters only";
+				return false;
+			}
+			string year = Value.Substring(0, 4);
+			if(year.CompareTo("1930") < 0)
+			{
+				err = "Date must be 19300101 or later";
+				return false;
+			}
+			DateTime now = DateTime.UtcNow;
+			string strNow = string.Format("{0:yyyyMMdd}", now);
+			if(Value.CompareTo(strNow) > 0)
+			{
+				err = "Date must not be later than today";
+				return false;
+			}
+			string month = Value.Substring(4, 2);
+			if(month.CompareTo("01") < 0 || month.CompareTo("12") > 0)
+			{
+				err = "Invalid month in date: must be between 01 and 12";
+				return false;
+			}
+			string day = Value.Substring(6, 2);
+			int lastDay = DateTime.DaysInMonth(int.Parse(year), int.Parse(month));
+			if(day.CompareTo("01") < 0 || day.CompareTo(lastDay.ToString()) > 0)
+			{
+				err = "Invalid day in date: must be between 01 and end of month";
+				return false;
+			}
+			return true;
+		}
+	}
+}
