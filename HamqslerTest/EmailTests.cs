422620db4b3642f58525cb38bb0c71a10570826e
Jim Orcheson <jimorcheson@gmail.com>
2013-09-15 11:52:19 -0400

    Added Email and EmailTests classes. Modified license.rtf to include statement about the source and licensing of the email regular expression used in Email.Validate.

---
+++ HamqslerTest/EmailTests.cs
@@ -0,0 +1,59 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for Email class
+	[TestFixture]
+	public class EmailTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			Email email = new Email("hamqsler@va3hj.ca");
+			Assert.AreEqual("<Email:17>hamqsler@va3hj.ca", email.ToAdifString());
+		}
+		
+		// test Validate with valid email address
+		[Test]
+		public void TestValidateValidAddress()
+		{
+			Email email = new Email("hamqsler@va3hj.ca");
+			string err = string.Empty;
+			Assert.IsTrue(email.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with invalid email address
+		[Test]
+		public void TestValidateInvalidAddress()
+		{
+			Email email = new Email("hamqsler.va3hj:ca");
+			string err = string.Empty;
+			Assert.IsFalse(email.Validate(out err));
+			Assert.AreEqual("'hamqsler.va3hj:ca' does not appear to be a valid email address.", err);
+		}
+		
+	}
+}
---
+++ hamqsler/Email.cs
@@ -0,0 +1,65 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Text.RegularExpressions;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// Email class - the contacted station's email address
+	/// </summary>
+	public class Email : StringField
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="addr">email address</param>
+		public Email(string addr) : base(addr)
+		{
+		}
+		
+		/// <summary>
+		/// Validate the email address
+		/// </summary>
+		/// <param name="err">Error info if not a valid email address</param>
+		/// <returns>true if valid, false if not</returns>
+		public override bool Validate(out string err)
+		{
+			err = null;
+			// the email regular expression below is copyright by Mykola Dobrochynskyy and
+			// released under Code Project Open License (CPOL) 1.02.
+			// See http://www.codeproject.com/info/cpol10.aspx for the terms of the license.
+			if(Regex.IsMatch(Value, @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+						     + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
+										[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+						     + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
+										[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+						     + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})$"))
+			{
+				return true;
+			}
+			else
+			{
+				err = string.Format("'{0}' does not appear to be a valid email address.", Value);
+				return  false;
+			}
+		}
+	}
+}
