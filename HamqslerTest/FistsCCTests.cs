a43b76a502371abbaeee0e647f3e5205ca97977f
Jim Orcheson <jimorcheson@gmail.com>
2013-09-17 08:15:57 -0400

    Added Fists_CC and FistsCCTests classes.

---
+++ HamqslerTest/FistsCCTests.cs
@@ -0,0 +1,69 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	//tests for Fists_CC class
+	[TestFixture]
+	public class FistsCCTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			Fists_CC fcc = new Fists_CC("1234");
+			Assert.AreEqual("<Fists_CC:4>1234", fcc.ToAdifString());
+		}
+		
+		// test Validate with valid CC number
+		[Test]
+		public void TestValidateValidCC()
+		{
+			Fists_CC fcc = new Fists_CC("1234");
+			string err = string.Empty;
+			Assert.IsTrue(fcc.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with invalid CC number
+		[Test]
+		public void TestValidateInvalidCC()
+		{
+			Fists_CC fcc = new Fists_CC("12f4");
+			string err = string.Empty;
+			Assert.IsFalse(fcc.Validate(out err));
+			Assert.AreEqual("Invalid Fists CC number.", err);
+		}
+		
+		// test Validate with invalid CC number
+		[Test]
+		public void TestValidateDecimalCC()
+		{
+			Fists_CC fcc = new Fists_CC("12.4");
+			string err = string.Empty;
+			Assert.IsFalse(fcc.Validate(out err));
+			Assert.AreEqual("Invalid Fists CC number.", err);
+		}
+		
+	}
+}
---
+++ hamqsler/Fists_CC.cs
@@ -0,0 +1,50 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Text.RegularExpressions;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// Fists_CC class - the contacted station's FISTS CW Club Century Certificate (CC) number, which
+	/// is a sequence of digits only.
+	/// </summary>
+	public class Fists_CC : NumberField
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="cc">Fists CC number</param>
+		public Fists_CC(string cc) : base(cc)
+		{
+		}
+		
+		public override bool Validate(out string err)
+		{
+			err = null;
+			if(!Regex.IsMatch(Value, "^[0-9]*$"))
+			{
+				err = "Invalid Fists CC number.";
+				return false;
+			}
+			return true;
+		}
+	}
+}
