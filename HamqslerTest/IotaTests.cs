92eef2d6e8d39872b6d62815fd11d6a71eb01ce5
Jim Orcheson <jimorcheson@gmail.com>
2013-09-17 09:24:33 -0400

    Added Iota and IotaTests classes.

---
+++ HamqslerTest/IotaTests.cs
@@ -0,0 +1,123 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using hamqsler;
+
+
+namespace hamqslerTest
+{
+	// tests for Iota class
+	[TestFixture]
+	public class IotaTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Iota iota = new Iota("NA-001", aEnums);
+			Assert.AreEqual("<Iota:6>NA-001", iota.ToAdifString());
+		}
+		
+		// test Validate with valid IOTA designator
+		[Test]
+		public void TestValidateValidDesignator()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Iota iota = new Iota("NA-001", aEnums);
+			string err = string.Empty;
+			Assert.IsTrue(iota.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with invalid IOTA designator
+		[Test]
+		public void TestValidateInvalidDesignator2()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Iota iota = new Iota("SNA-001", aEnums);
+			string err = string.Empty;
+			Assert.IsFalse(iota.Validate(out err));
+			Assert.AreEqual("'SNA-001' is not a valid IOTA designator.", err);
+		}
+		
+		// test Validate with invalid IOTA designator
+		[Test]
+		public void TestValidateInvalidDesignator3()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Iota iota = new Iota("NA-0011", aEnums);
+			string err = string.Empty;
+			Assert.IsFalse(iota.Validate(out err));
+			Assert.AreEqual("'NA-0011' is not a valid IOTA designator.", err);
+		}
+		
+		// test Validate with invalid IOTA designator
+		[Test]
+		public void TestValidateInvalidDesignator4()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Iota iota = new Iota("NA-0F1", aEnums);
+			string err = string.Empty;
+			Assert.IsFalse(iota.Validate(out err));
+			Assert.AreEqual("'NA-0F1' is not a valid IOTA designator.", err);
+		}
+		
+		// test Validate with invalid IOTA designator (bad format)
+		[Test]
+		public void TestValidateBadFormat()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Iota iota = new Iota("BA0001", aEnums);
+			string err = string.Empty;
+			Assert.IsFalse(iota.Validate(out err));
+			Assert.AreEqual("'BA0001' is not a valid IOTA designator.", err);
+		}
+		
+		// test Validate with invalid IOTA designator (bad continent
+		[Test]
+		public void TestValidateBadContinent()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Iota iota = new Iota("BA-001", aEnums);
+			string err = string.Empty;
+			Assert.IsFalse(iota.Validate(out err));
+			Assert.AreEqual("'BA-001' is not a valid IOTA designator.", err);
+		}
+		
+	}
+}
---
+++ hamqsler/Iota.cs
@@ -0,0 +1,65 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Text.RegularExpressions;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// Iota class - The contacted station's IOTA designator, in format CC-XXX where
+	/// CC is a member of the Continent enumeration, and XXX is the island designator
+	/// with 000 < = XXX <c= 999
+	/// </summary>
+	public class Iota : StringField
+	{
+		private AdifEnumerations adifEnums = null;
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="iota">IOTA designator</param>
+		/// <param name="aEnums">AdifEnumerations object containing Continent enumeration</param>
+		public Iota(string iota, AdifEnumerations aEnums) : base(iota)
+		{
+			adifEnums = aEnums;
+		}
+		
+		/// <summary>
+		/// Validate the value - must be valid IOTA designator
+		/// </summary>
+		/// <param name="err">Error message if invalid, null otherwise</param>
+		/// <returns>true if designator is in valid format, false otherwise</returns>
+		public override bool Validate(out string err)
+		{
+			err = null;
+			if(!Regex.IsMatch(Value, "^[A-Z]{2}-[0-9]{3}$"))
+			{
+				err = string.Format("'{0}' is not a valid IOTA designator.", Value);
+				return false;
+			}
+			string[] parts = Value.Split('-');
+			if(!adifEnums.IsInEnumeration("Continent", parts[0]))
+			{
+				err = string.Format("'{0}' is not a valid IOTA designator.", Value);
+				return false;
+			}
+			return true;
+		}
+	}
+}
