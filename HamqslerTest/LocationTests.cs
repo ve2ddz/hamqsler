254eaa9e3c317625120b269ca098b046f27ab350
Jim Orcheson <jimorcheson@gmail.com>
2013-09-17 11:06:14 -0400

    Added Location and LocationTests classes.

---
+++ HamqslerTest/LocationTests.cs
@@ -0,0 +1,149 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for Location class
+	[TestFixture]
+	public class LocationTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			Location loc = new Location("W079 54.086");
+			Assert.AreEqual("<Location:11>W079 54.086", loc.ToAdifString());
+		}
+
+		// test Validate with valid location
+		[Test]
+		public void TestValidateValidLocation()
+		{
+			Location loc = new Location("W079 54.086");
+			string err = string.Empty;
+			Assert.IsTrue(loc.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+
+		// test Validate with invalid direction
+		[Test]
+		public void TestValidateInvalidDirection()
+		{
+			Location loc = new Location("U079 54.086");
+			string err = string.Empty;
+			Assert.IsFalse(loc.Validate(out err));
+			Assert.AreEqual("'U079 54.086' is not a valid location.", err);
+		}
+
+		// test Validate with invalid location format
+		[Test]
+		public void TestValidateInvalidLocation()
+		{
+			Location loc = new Location("S07 54.086");
+			string err = string.Empty;
+			Assert.IsFalse(loc.Validate(out err));
+			Assert.AreEqual("'S07 54.086' is not a valid location.", err);
+		}
+
+		// test Validate with invalid location format
+		[Test]
+		public void TestValidateInvalidLocation1()
+		{
+			Location loc = new Location("S079  54.086");
+			string err = string.Empty;
+			Assert.IsFalse(loc.Validate(out err));
+			Assert.AreEqual("'S079  54.086' is not a valid location.", err);
+		}
+
+		// test Validate with invalid location format
+		[Test]
+		public void TestValidateInvalidLocation2()
+		{
+			Location loc = new Location("S079 4.086");
+			string err = string.Empty;
+			Assert.IsFalse(loc.Validate(out err));
+			Assert.AreEqual("'S079 4.086' is not a valid location.", err);
+		}
+
+		// test Validate with invalid location format
+		[Test]
+		public void TestValidateInvalidLocation3()
+		{
+			Location loc = new Location("S079 543.08");
+			string err = string.Empty;
+			Assert.IsFalse(loc.Validate(out err));
+			Assert.AreEqual("'S079 543.08' is not a valid location.", err);
+		}
+
+		// test Validate with invalid location format
+		[Test]
+		public void TestValidateInvalidLocation4()
+		{
+			Location loc = new Location("S079 54.0866");
+			string err = string.Empty;
+			Assert.IsFalse(loc.Validate(out err));
+			Assert.AreEqual("'S079 54.0866' is not a valid location.", err);
+		}
+
+		// test Validate with invalid location format
+		[Test]
+		public void TestValidateInvalidLocation5()
+		{
+			Location loc = new Location("S079 54.0f6");
+			string err = string.Empty;
+			Assert.IsFalse(loc.Validate(out err));
+			Assert.AreEqual("'S079 54.0f6' is not a valid location.", err);
+		}
+
+		// test Validate with invalid location format
+		[Test]
+		public void TestValidateInvalidLocation7()
+		{
+			Location loc = new Location("S181 54.086");
+			string err = string.Empty;
+			Assert.IsFalse(loc.Validate(out err));
+			Assert.AreEqual("'S181 54.086' is not a valid location.", err);
+		}
+
+		// test Validate with invalid location format
+		[Test]
+		public void TestValidateInvalidLocation8()
+		{
+			Location loc = new Location("S073 60.000");
+			string err = string.Empty;
+			Assert.IsFalse(loc.Validate(out err));
+			Assert.AreEqual("'S073 60.000' is not a valid location.", err);
+		}
+
+		// test Validate with invalid location format
+		[Test]
+		public void TestValidateInvalidLocation9()
+		{
+			Location loc = new Location("S180 00.001");
+			string err = string.Empty;
+			Assert.IsFalse(loc.Validate(out err));
+			Assert.AreEqual("'S180 00.001' is not a valid location.", err);
+		}
+
+	}
+}
---
+++ hamqsler/Location.cs
@@ -0,0 +1,88 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// Location class - a sequence of characters representing a latitude or longitude in
+	/// XDDD MM.MMM format, where X is a directional character from the set {E, W, N, S}
+	/// DDD is a 3 digit degrees specifier (0 <= DDD <= 180), and
+	/// MM.MMM is a 6 digit minutes specifier (0 <= MM.MMM <= 59.999)
+	/// </summary>
+	public class Location: StringField
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="location">location specifier</param>
+		public Location(string location) : base(location)
+		{
+		}
+		
+		/// <summary>
+		/// Validate that location is in correct format
+		/// </summary>
+		/// <param name="err">Error message if location not in correct format, null otherwise</param>
+		/// <returns>true if correct format, false otherwise</returns>
+		public override bool Validate(out string err)
+		{
+			err = null;
+			if(!Regex.IsMatch(Value, @"^[EWNS][0-9]{3} [0-9]{2}\.[0-9]{3}$"))
+			{
+				err = string.Format("'{0}' is not a valid location.", Value);
+				return false;
+			}
+			string deg = Value.Substring(1, 3);
+			int degrees = 0;
+			if(!Int32.TryParse(deg, NumberStyles.None, CultureInfo.InvariantCulture, out degrees))
+			{
+				err = string.Format("'{0}' is not a valid location.", Value);
+				return false;				
+			}
+			if(degrees > 180)
+			{
+				err = string.Format("'{0}' is not a valid location.", Value);
+				return false;				
+			}
+			string mins = Value.Substring(5, 6);
+			float minutes = 0F;
+			if(!float.TryParse(mins, NumberStyles.Any, CultureInfo.InvariantCulture,
+			                  out minutes))
+			{
+				err = string.Format("'{0}' is not a valid location.", Value);
+				return false;
+			}
+			if(minutes >= 60F)
+			{
+				err = string.Format("'{0}' is not a valid location.", Value);
+				return false;
+			}
+			if(degrees == 180 && minutes != 0F)
+			{
+				err = string.Format("'{0}' is not a valid location.", Value);
+				return false;
+			}
+			return true;
+		}
+	}
+}
