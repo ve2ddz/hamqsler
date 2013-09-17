9980a54092c0143e0be4da3b582d22235d27ae61
Jim Orcheson <jimorcheson@gmail.com>
2013-09-17 11:20:38 -0400

    Added Lat and LatTests classes.

---
+++ HamqslerTest/LatTests.cs
@@ -0,0 +1,38 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for Lat class
+	[TestFixture]
+	public class LatTests
+	{
+		// test AdifToString
+		[Test]
+		public void TestAdifToString()
+		{
+			Lat lat = new Lat("S054 23.456");
+			Assert.AreEqual("<Lat:11>S054 23.456", lat.ToAdifString());
+		}
+	}
+}
