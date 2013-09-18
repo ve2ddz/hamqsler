c4c193a2c6d03602c2b2a2049c7a145ff2a53ced
Jim Orcheson <jimorcheson@gmail.com>
2013-09-18 08:19:26 -0400

    Added My_Cnty and MyCntyTests classes

---
+++ hamqsler/My_Cnty.cs
@@ -0,0 +1,36 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// My_Cnty class - the logging station's Secondary Administrative Subdivision
+	/// (e.g. US County, JA Gun), in format specified in ADIF specification
+	/// Note: Secondary Administrative Subdivisions are not enumerated in the ADIF specification.
+	/// Therefore, no actual validity checking is performed.
+	/// </summary>
+	public class My_Cnty : StringField
+	{
+		public My_Cnty(string cnty) : base(cnty)
+		{
+		}
+	}
+}
