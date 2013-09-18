49f83871579c0a5cf3f3d17e0525de6d49625072
Jim Orcheson <jimorcheson@gmail.com>
2013-09-18 09:40:39 -0400

    Added My_Iota_Island_ID and MyIotaIslandIdTests classes.

---
+++ HamqslerTest/MyIotaIslandIdTests.cs
@@ -0,0 +1,38 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for My_Iota_Island_ID class
+	[TestFixture]
+	public class MyIotaIslandIdTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			My_Iota_Island_ID id = new My_Iota_Island_ID("An ID");
+			Assert.AreEqual("<My_Iota_Island_ID:5>An ID", id.ToAdifString());
+		}
+	}
+}
