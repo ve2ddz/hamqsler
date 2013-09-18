795f6a5f8a76f0ef2aad172616e43c87a684a3e3
Jim Orcheson <jimorcheson@gmail.com>
2013-09-18 10:03:19 -0400

    Added My_Postal_Code and MyPostalCodeTests classes.

---
+++ HamqslerTest/MyPostalCodeTests.cs
@@ -0,0 +1,38 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for My_Postal_Code class
+	[TestFixture]
+	public class MyPostalCodeTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			My_Postal_Code code = new My_Postal_Code("N6P 1F7");
+			Assert.AreEqual("<My_Postal_Code:7>N6P 1F7", code.ToAdifString());
+		}
+	}
+}
