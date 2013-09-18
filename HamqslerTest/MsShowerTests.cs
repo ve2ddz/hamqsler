09b2ccb17bdeffece734d30ad02b5a1304fe58bd
Jim Orcheson <jimorcheson@gmail.com>
2013-09-18 08:05:31 -0400

    Added Ms_Shower and MsShowerTests classes.

---
+++ HamqslerTest/MsShowerTests.cs
@@ -0,0 +1,38 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for Ms_Shower class
+	[TestFixture]
+	public class MsShowerTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			Ms_Shower shower = new Ms_Shower("Perseids");
+			Assert.AreEqual("<Ms_Shower:8>Perseids", shower.ToAdifString());
+		}
+	}
+}
