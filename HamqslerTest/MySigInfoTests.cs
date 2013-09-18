ed1626f282fbbc4f3367325c510d1b6c72ecf120
Jim Orcheson <jimorcheson@gmail.com>
2013-09-18 10:30:10 -0400

    Added My_Sig_Info and MySigInfoTests classes.

---
+++ HamqslerTest/MySigInfoTests.cs
@@ -0,0 +1,38 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for My_Sig_Info class
+	[TestFixture]
+	public class MySigInfoTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			My_Sig_Info info = new My_Sig_Info("Info for my SIG");
+			Assert.AreEqual("<My_Sig_Info:15>Info for my SIG", info.ToAdifString());
+		}
+	}
+}
