68b5d73ec127ab56326ebd7a51576af6c1cf0668
Jim Orcheson <jimorcheson@gmail.com>
2013-09-19 08:22:08 -0400

    Added VUCC_Grids and VUCCGridsTests classes.

---
+++ HamqslerTest/VUCCGridsTests.cs
@@ -0,0 +1,155 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for VUCC_Grids class
+	[TestFixture]
+	public class VUCCGridsTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			VUCC_Grids grids = new VUCC_Grids("EN98,FM08,EM97,FM07");
+			Assert.AreEqual("<VUCC_Grids:19>EN98,FM08,EM97,FM07", grids.ToAdifString());
+		}
+		
+		// test Validate with two valid grid squares
+		[Test]
+		public void TestValidate2Valid()
+		{
+			VUCC_Grids grids = new VUCC_Grids("EN98,EN97");
+			string err = string.Empty;
+			Assert.IsTrue(grids.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with four valid grid squares
+		[Test]
+		public void TestValidate4Valid()
+		{
+			VUCC_Grids grids = new VUCC_Grids("EN98,EN97,FM08,FM07");
+			string err = string.Empty;
+			Assert.IsTrue(grids.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with one valid grid square
+		[Test]
+		public void TestValidate1Valid()
+		{
+			VUCC_Grids grids = new VUCC_Grids("EN98");
+			string err = string.Empty;
+			Assert.IsFalse(grids.Validate(out err));
+			Assert.AreEqual("VUCC_Grids contains 1 grid squares. Must contain either 2 or 4 grid squares.", 
+			                err);
+		}
+		
+		// test Validate with 0 grid squares
+		[Test]
+		public void TestValidate0Valid()
+		{
+			VUCC_Grids grids = new VUCC_Grids(string.Empty);
+			string err = string.Empty;
+			Assert.IsTrue(grids.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with invalid grid square
+		[Test]
+		public void TestValidateInvalid()
+		{
+			VUCC_Grids grids = new VUCC_Grids("EN9,EN97");
+			string err = string.Empty;
+			Assert.IsFalse(grids.Validate(out err));
+			Assert.AreEqual("'EN9' is not a valid VUCC_Grids grid square", 
+			                err);
+		}
+		
+		// test Validate with invalid grid square
+		[Test]
+		public void TestValidateInvalid1()
+		{
+			VUCC_Grids grids = new VUCC_Grids("EN98,EN9");
+			string err = string.Empty;
+			Assert.IsFalse(grids.Validate(out err));
+			Assert.AreEqual("'EN9' is not a valid VUCC_Grids grid square", 
+			                err);
+		}
+		
+		// test Validate with invalid grid square
+		[Test]
+		public void TestValidateInvalid2()
+		{
+			VUCC_Grids grids = new VUCC_Grids("EN98,EN97bf");
+			string err = string.Empty;
+			Assert.IsFalse(grids.Validate(out err));
+			Assert.AreEqual("'EN97bf' is not a valid VUCC_Grids grid square", 
+			                err);
+		}
+		
+		// test Validate with invalid grid square
+		[Test]
+		public void TestValidateInvalid3()
+		{
+			VUCC_Grids grids = new VUCC_Grids("AX98,EN97");
+			string err = string.Empty;
+			Assert.IsFalse(grids.Validate(out err));
+			Assert.AreEqual("'AX98' is not a valid VUCC_Grids grid square", 
+			                err);
+		}
+		
+		// test Validate with invalid grid square
+		[Test]
+		public void TestValidateInvalid4()
+		{
+			VUCC_Grids grids = new VUCC_Grids("9A98,EN97");
+			string err = string.Empty;
+			Assert.IsFalse(grids.Validate(out err));
+			Assert.AreEqual("'9A98' is not a valid VUCC_Grids grid square", 
+			                err);
+		}
+		
+		// test Validate with invalid grid square
+		[Test]
+		public void TestValidateInvalid5()
+		{
+			VUCC_Grids grids = new VUCC_Grids("EN9B,EN97");
+			string err = string.Empty;
+			Assert.IsFalse(grids.Validate(out err));
+			Assert.AreEqual("'EN9B' is not a valid VUCC_Grids grid square", 
+			                err);
+		}
+		
+		// test Validate with valid grid squares with spaces
+		[Test]
+		public void TestValidateSpaces()
+		{
+			VUCC_Grids grids = new VUCC_Grids("EN98 , EN97");
+			string err = string.Empty;
+			Assert.IsTrue(grids.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+	}
+}
---
+++ hamqsler/VUCC_Grids.cs
@@ -0,0 +1,94 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Text.RegularExpressions;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// VUCC_Grids class - Two or four adjacent Maidenhead grid locators, each 4 characters long,
+	/// representing the contacted station's grid squares cardited to the QSO for the ARRL
+	/// VUCC program (e.g. EN98,FM08,EM97,FM07)
+	/// </summary>
+	public class VUCC_Grids : AdifField
+	{
+		private DelimitedList gridSquares = null;
+
+		public int Count
+		{
+			get {return gridSquares.Count;}
+		}
+		
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="grids">list of two or four grid squares</param>
+		public VUCC_Grids(string grids)
+		{
+			string[] gs = grids.Split(',');
+			string grds = string.Empty;
+			char[] trimChars = new char[1];
+			trimChars[0] = ' ';
+			foreach(string grid in gs)
+			{
+				string gr = grid.Trim(trimChars);
+				grds += gr + ",";
+			}
+			grds = grds.Substring(0, grds.Length - 1);
+			gridSquares = new DelimitedList(',', grds);
+		}
+		
+		/// <summary>
+		/// Get VUCC_Grids in Adif Field format
+		/// </summary>
+		/// <returns>VUCC_Grids in Adif Field format</returns>
+		public string ToAdifString()
+		{
+			string squares = gridSquares.ToString();
+			return string.Format("<{0}:{1}>{2}", base.Name, squares.Length, squares);
+		}
+		
+		/// <summary>
+		/// Validate the grid squares
+		/// </summary>
+		/// <param name="err">Error message if one or more grid squares is not valid, null otherwise</param>
+		/// <returns>true if 0, 2 or 4 grid squares and grid squares are in correct format</returns>
+		public override bool Validate(out string err)
+		{
+			err = null;
+			if(!(Count == 2) && !(Count == 4) && !(Count == 0))
+			{
+				err = string.Format("{1} contains {0} grid squares. Must contain either 2 or 4 grid squares.",
+				                    Count, base.Name);
+				return false;
+			}
+			foreach(string grid in gridSquares.Items)
+			{
+				if(!Regex.IsMatch(grid, "^[A-Ra-r][A-Ra-r][0-9][0-9]$"))
+				{
+					err = string.Format("'{0}' is not a valid {1} grid square",
+					                    grid, base.Name);
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
