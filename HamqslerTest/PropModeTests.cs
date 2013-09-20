840df33e6e4eb544451f90237aaa958884152db2
Jim Orcheson <jimorcheson@gmail.com>
2013-09-20 07:02:17 -0400

    Added Propagation enumeration to AdifEnumerations.xml. Added Prop_Mode.cs and PropModeTests classes.

---
+++ HamqslerTest/PropModeTests.cs
@@ -0,0 +1,70 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for Prop_Mode class
+	[TestFixture]
+	public class PropModeTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Prop_Mode pm = new Prop_Mode("EME", aEnums);
+			Assert.AreEqual("<Prop_Mode:3>EME", pm.ToAdifString());
+		}
+
+		// test Validate with valid mode
+		[Test]
+		public void TestTValidateValidMode()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Prop_Mode pm = new Prop_Mode("EME", aEnums);
+			string err = string.Empty;
+			Assert.IsTrue(pm.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+
+		// test Validate with invalid mode
+		[Test]
+		public void TestTValidateInvalidMode()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Prop_Mode pm = new Prop_Mode("PMS", aEnums);
+			string err = string.Empty;
+			Assert.IsFalse(pm.Validate(out err));
+			Assert.AreEqual("This QSO Field is of type enumeration. The value 'PMS' was not found in enumeration",
+			                err);
+		}
+	}
+}
