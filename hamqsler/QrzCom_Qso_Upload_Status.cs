d5550e73c6afdb8d904bcef6f1fc276f4c79e65c
Jim Orcheson <jimorcheson@gmail.com>
2013-09-20 07:21:56 -0400

    Added QrzCom_Qso_Upload_Status and QrzComQsoUploadStatusTests classes.

---
+++ hamqsler/QrzCom_Qso_Upload_Status.cs
@@ -0,0 +1,34 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// QrzCom_Qso_Upload_Status class - upload status of the QSO on the QRZ.COM online service
+	/// </summary>
+	public class QrzCom_Qso_Upload_Status : EnumerationValue
+	{
+		public QrzCom_Qso_Upload_Status(string status, AdifEnumerations aEnums)
+			: base(status, "Qso_Upload_Status", aEnums)
+		{
+		}
+	}
+}
