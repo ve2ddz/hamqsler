67191f215ebf5f30418ac430db3aabce687e0813
Jim Orcheson <jimorcheson@gmail.com>
2013-09-20 10:38:50 -0400

    Added Rst_Sent and RstSentTests classes.

---
+++ HamqslerTest/RstSentTests.cs
@@ -0,0 +1,48 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for Rst_Sent class
+	[TestFixture]
+	public class RstSentTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			Rst_Sent rst = new Rst_Sent("599");
+			Assert.AreEqual("<Rst_Sent:3>599", rst.ToAdifString());
+		}
+
+		// test Validate - any value is allowed
+		[Test]
+		public void TestValidate()
+		{
+			Rst_Sent rst = new Rst_Sent("599");
+			string err = string.Empty;
+			Assert.IsTrue(rst.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+	}
+}
