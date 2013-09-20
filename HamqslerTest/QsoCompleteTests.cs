1e2e39e010cbb247da24a0d4cacfdd4c65f63684
Jim Orcheson <jimorcheson@gmail.com>
2013-09-20 09:59:46 -0400

    Added Qso_Complete enumeration to AdifEnumerations.xml. Added Qso_Complete and QsoCompleteTests classes.

---
+++ HamqslerTest/QsoCompleteTests.cs
@@ -0,0 +1,70 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for Qso_Complete class
+	[TestFixture]
+	public class QsoCompleteTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Qso_Complete qc = new Qso_Complete("NIL", aEnums);
+			Assert.AreEqual("<Qso_Complete:3>NIL", qc.ToAdifString());
+		}
+
+		// test Validate with valid value
+		[Test]
+		public void TestValidateValidValue()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Qso_Complete qc = new Qso_Complete("?", aEnums);
+			string err = string.Empty;
+			Assert.IsTrue(qc.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+
+		// test Validate with invalid value
+		[Test]
+		public void TestValidateInvalidValue()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			AdifEnumerations aEnums = new AdifEnumerations(str);
+			Qso_Complete qc = new Qso_Complete("F", aEnums);
+			string err = string.Empty;
+			Assert.IsFalse(qc.Validate(out err));
+			Assert.AreEqual("This QSO Field is of type enumeration. The value 'F' was not found in enumeration",
+			                err);
+		}
+	}
+}
