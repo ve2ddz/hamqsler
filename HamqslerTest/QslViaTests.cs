0b7e17272a9e2e3615089576cd4d9523215e16d8
Jim Orcheson <jimorcheson@gmail.com>
2013-09-20 08:27:43 -0400

    Added Qsl_Via and QslViaTests classes.

---
+++ HamqslerTest/QslViaTests.cs
@@ -0,0 +1,48 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for Qsl_Via class
+	[TestFixture]
+	public class QslViaTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			Qsl_Via via = new Qsl_Via("LotW and eQsl.cc");
+			Assert.AreEqual("<Qsl_Via:16>LotW and eQsl.cc", via.ToAdifString());
+		}
+		
+		// test Validate - any value is acceptable
+		[Test]
+		public void TestValidate()
+		{
+			Qsl_Via via = new Qsl_Via("LotW and eQsl.cc");
+			string err = string.Empty;
+			Assert.IsTrue(via.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+	}
+}
---
+++ hamqsler/Qsl_Via.cs
@@ -0,0 +1,39 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// Qsl_Via class - the contacted station's QSL route.
+	/// The value is used elsewhere in HamQSLer to contain the callsign of the contacted
+	/// station's manager
+	/// </summary>
+	public class Qsl_Via : StringField
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="via">QSL route</param>
+		public Qsl_Via(string via) : base(via)
+		{
+		}
+	}
+}
