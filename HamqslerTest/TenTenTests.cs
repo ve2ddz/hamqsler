e96f8a676e325a8513606e38f214fb2ce593feae
Jim Orcheson <jimorcheson@gmail.com>
2013-09-23 08:29:13 -0400

    Added Ten_Ten and TenTenTests classes.

---
+++ HamqslerTest/TenTenTests.cs
@@ -0,0 +1,38 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for Ten_Ten class
+	[TestFixture]
+	public class TenTenTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			Ten_Ten tt = new Ten_Ten("146");
+			Assert.AreEqual("<Ten_Ten:3>146", tt.ToAdifString());
+		}
+	}
+}
