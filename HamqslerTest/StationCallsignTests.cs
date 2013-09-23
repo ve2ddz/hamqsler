8fc77fa7fbad1dea46a3103e2406bc7443bbfa8a
Jim Orcheson <jimorcheson@gmail.com>
2013-09-23 07:04:30 -0400

    Added Station_Callsign and StationCallsignTests classes.

---
+++ HamqslerTest/StationCallsignTests.cs
@@ -0,0 +1,68 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for Station_Callsign class
+	[TestFixture]
+	public class StationCallsignTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			Station_Callsign oCall = new Station_Callsign("VA3HJ");
+			Assert.AreEqual("<Station_Callsign:5>VA3HJ", oCall.ToAdifString());
+		}
+		
+		// test Validate with valid callsign
+		[Test]
+		public void TestValidateValidCall()
+		{
+			Station_Callsign oCall = new Station_Callsign("VA3HJ");
+			string err = string.Empty;
+			Assert.IsTrue(oCall.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with invalid callsign
+		[Test]
+		public void TestValidateInvalidCall()
+		{
+			Station_Callsign oCall = new Station_Callsign("VAHJ");
+			string err = string.Empty;
+			Assert.IsFalse(oCall.Validate(out err));
+			Assert.AreEqual("Callsign 'VAHJ' is invalid.", err);
+		}
+		
+		// test Validate with compound callsign
+		[Test]
+		public void TestValidateCompoundCall()
+		{
+			Station_Callsign oCall = new Station_Callsign("VA3HJ/W8");
+			string err = string.Empty;
+			Assert.IsFalse(oCall.Validate(out err));
+			Assert.AreEqual("Callsign 'VA3HJ/W8' contains modifiers.", err);
+		}
+	}
+}
---
+++ hamqsler/Station_Callsign.cs
@@ -0,0 +1,58 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Text.RegularExpressions;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// Station_Callsign class - the logging station's callsign (the callsign used over the air)
+	/// </summary>
+	public class Station_Callsign : Call
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="call">callsign</param>
+		public Station_Callsign(string call) : base(call)
+		{
+		}
+
+		/// <summary>
+		/// Validate the callsign. Must be simple call in valid format
+		/// </summary>
+		/// <param name="err">Error message if call is invalid, null otherwise</param>
+		/// <returns>true if call is valid, false otherwise</returns>
+		public override bool Validate(out string err)
+		{
+			err = null;
+			if(!base.Validate(out err))
+			{
+				return false;
+			}
+			if(!Regex.IsMatch(Value, "^[A-Za-z0-9]+$"))
+			{
+				err = string.Format("Callsign '{0}' contains modifiers.", Value);
+				return false;
+			}
+			return true;
+		}
+	}
+}
