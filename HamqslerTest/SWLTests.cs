b9dc69fdbcff1a928330f8fdc75cac58fa4d2232
Jim Orcheson <jimorcheson@gmail.com>
2013-09-23 08:21:00 -0400

    Added SWL and SWLTests classes.

---
+++ HamqslerTest/SWLTests.cs
@@ -0,0 +1,68 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for SWL class
+	[TestFixture]
+	public class SWLTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			SWL swl = new SWL("Y");
+			Assert.AreEqual("<SWL:1>Y", swl.ToAdifString());
+		}
+
+		// test Validate with valid value
+		[Test]
+		public void TestValidateY()
+		{
+			SWL swl = new SWL("Y");
+			string err = string.Empty;
+			Assert.IsTrue(swl.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with valid value
+		[Test]
+		public void TestValidateN()
+		{
+			SWL swl = new SWL("N");
+			string err = string.Empty;
+			Assert.IsTrue(swl.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with invalid value
+		[Test]
+		public void TestValidateInvalidValue()
+		{
+			SWL swl = new SWL("F");
+			string err = string.Empty;
+			Assert.IsFalse(swl.Validate(out err));
+			Assert.AreEqual("Boolean field must have value 'Y' or 'N'.", err);
+		}
+	}
+}
