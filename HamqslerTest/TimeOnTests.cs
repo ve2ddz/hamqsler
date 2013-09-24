0eb258f23011428340c02e1a743e65759b491699
Jim Orcheson <jimorcheson@gmail.com>
2013-09-24 06:14:38 -0400

    Added Time_On and TimeOnTests classes.

---
+++ HamqslerTest/TimeOnTests.cs
@@ -0,0 +1,148 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for Time_On class
+	[TestFixture]
+	public class TimeOnTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			Time_On tf = new Time_On("123456");
+			Assert.AreEqual("<Time_On:6>123456", tf.ToAdifString());
+		}
+		
+		// test Validate with empty time
+		[Test]
+		public void TestValidateValidEmpty()
+		{
+			Time_On tf = new Time_On("");
+			string err = string.Empty;
+			Assert.IsTrue(tf.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with valid HHMM time
+		[Test]
+		public void TestValidateValidHHMM()
+		{
+			Time_On tf = new Time_On("1234");
+			string err = string.Empty;
+			Assert.IsTrue(tf.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with valid HHMMSS time
+		[Test]
+		public void TestValidateValidHHMMSS()
+		{
+			Time_On tf = new Time_On("123456");
+			string err = string.Empty;
+			Assert.IsTrue(tf.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with invalid HHMMS time
+		[Test]
+		public void TestValidateInvalidHHM()
+		{
+			Time_On tf = new Time_On("123");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Time must be in HHMM or HHMMSS format.", err);
+		}
+		
+		// test Validate with invalid HHMMS time
+		[Test]
+		public void TestValidateInvalidHHMMS()
+		{
+			Time_On tf = new Time_On("12345");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Time must be in HHMM or HHMMSS format.", err);
+		}
+		
+		// test Validate with invalid hour
+		[Test]
+		public void TestValidateInvalidHour()
+		{
+			Time_On tf = new Time_On("243456");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Invalid time.", err);
+		}
+		
+		// test Validate with invalid minute
+		[Test]
+		public void TestValidateInvalidMinute()
+		{
+			Time_On tf = new Time_On("126056");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Time must be in HHMM or HHMMSS format.", err);
+		}
+		
+		// test Validate with invalid hour
+		[Test]
+		public void TestValidateInvalidHour2()
+		{
+			Time_On tf = new Time_On("2434");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Invalid time.", err);
+		}
+		
+		// test Validate with invalid minute
+		[Test]
+		public void TestValidateInvalidMinute2()
+		{
+			Time_On tf = new Time_On("1260");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Time must be in HHMM or HHMMSS format.", err);
+		}
+		
+		// test Validate with invalid second
+		[Test]
+		public void TestValidateInvalidSecond()
+		{
+			Time_On tf = new Time_On("125060");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Time must be in HHMM or HHMMSS format.", err);
+		}
+		
+		// test Validate with non digit
+		[Test]
+		public void TestValidateNonDigit()
+		{
+			Time_On tf = new Time_On("1250-0");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Time must be in HHMM or HHMMSS format.", err);
+		}
+	}
+}
