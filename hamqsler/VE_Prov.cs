fc2355d8885afcaf35f1520fd4bb335f4342ca3e
Jim Orcheson <jimorcheson@gmail.com>
2013-09-24 06:28:42 -0400

    Added VE_Prov and VEProvTests classes.

---
+++ hamqsler/VE_Prov.cs
@@ -0,0 +1,46 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// VE_Prov class - deprecated. Use State instead
+	/// </summary>
+	public class VE_Prov : StringField
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="prov">Canadian province</param>
+		public VE_Prov(string prov) : base(prov)
+		{
+		}
+		
+		/// <summary>
+		/// Output class as a string in ADIF format
+		/// </summary>
+		/// <returns>value in ADIF format</returns>
+		public override string ToAdifString()
+		{
+			return string.Format("<State:{0}>{1}", Value.Length, Value);
+		}
+	}
+}
