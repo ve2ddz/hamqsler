0c44a94f2532d2f7334e504bf21b140c0c987a1b
Jim Orcheson <jimorcheson@gmail.com>
2013-09-24 06:04:30 -0400

    Added TimeField and TimeFieldTests classes.

---
+++ HamqslerTest/TimeFieldTests.cs
@@ -0,0 +1,148 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for TimeField class
+	[TestFixture]
+	public class TimeFieldTests
+	{
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			TimeField tf = new TimeField("123456");
+			Assert.AreEqual("<TimeField:6>123456", tf.ToAdifString());
+		}
+		
+		// test Validate with empty time
+		[Test]
+		public void TestValidateValidEmpty()
+		{
+			TimeField tf = new TimeField("");
+			string err = string.Empty;
+			Assert.IsTrue(tf.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with valid HHMM time
+		[Test]
+		public void TestValidateValidHHMM()
+		{
+			TimeField tf = new TimeField("1234");
+			string err = string.Empty;
+			Assert.IsTrue(tf.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with valid HHMMSS time
+		[Test]
+		public void TestValidateValidHHMMSS()
+		{
+			TimeField tf = new TimeField("123456");
+			string err = string.Empty;
+			Assert.IsTrue(tf.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with invalid HHMMS time
+		[Test]
+		public void TestValidateInvalidHHM()
+		{
+			TimeField tf = new TimeField("123");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Time must be in HHMM or HHMMSS format.", err);
+		}
+		
+		// test Validate with invalid HHMMS time
+		[Test]
+		public void TestValidateInvalidHHMMS()
+		{
+			TimeField tf = new TimeField("12345");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Time must be in HHMM or HHMMSS format.", err);
+		}
+		
+		// test Validate with invalid hour
+		[Test]
+		public void TestValidateInvalidHour()
+		{
+			TimeField tf = new TimeField("243456");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Invalid time.", err);
+		}
+		
+		// test Validate with invalid minute
+		[Test]
+		public void TestValidateInvalidMinute()
+		{
+			TimeField tf = new TimeField("126056");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Time must be in HHMM or HHMMSS format.", err);
+		}
+		
+		// test Validate with invalid hour
+		[Test]
+		public void TestValidateInvalidHour2()
+		{
+			TimeField tf = new TimeField("2434");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Invalid time.", err);
+		}
+		
+		// test Validate with invalid minute
+		[Test]
+		public void TestValidateInvalidMinute2()
+		{
+			TimeField tf = new TimeField("1260");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Time must be in HHMM or HHMMSS format.", err);
+		}
+		
+		// test Validate with invalid second
+		[Test]
+		public void TestValidateInvalidSecond()
+		{
+			TimeField tf = new TimeField("125060");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Time must be in HHMM or HHMMSS format.", err);
+		}
+		
+		// test Validate with non digit
+		[Test]
+		public void TestValidateNonDigit()
+		{
+			TimeField tf = new TimeField("1250-0");
+			string err = string.Empty;
+			Assert.IsFalse(tf.Validate(out err));
+			Assert.AreEqual("Time must be in HHMM or HHMMSS format.", err);
+		}
+	}
+}
---
+++ hamqsler/TimeField.cs
@@ -0,0 +1,65 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Text.RegularExpressions;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// TimeField class - base class for ADIF time classes
+	/// </summary>
+	public class TimeField : StringField
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="time">time in HHMM or HHMMSS format</param>
+		public TimeField(string time) : base(time)
+		{
+		}
+		
+		/// <summary>
+		/// Validate time field
+		/// </summary>
+		/// <param name="err">Error message if time is invalid, null otherwise</param>
+		/// <returns>true if time is valid, false otherwise.</returns>
+		public override bool Validate(out string err)
+		{
+			err = null;
+			int length = Value.Length;
+			if(length == 0)
+			{
+				return true;
+			}
+			if(!Regex.IsMatch(Value, "^[0-2][0-9][0-5][0-9]$|^[0-2][0-9][0-5][0-9][0-5][0-9]$"))
+			{
+				err = "Time must be in HHMM or HHMMSS format.";
+				return false;
+			}
+			string hour = Value.Substring(0, 2);
+			if(hour.CompareTo("24") >= 0)
+			{
+				err = "Invalid time.";
+				return false;
+			}
+			return true;
+		}
+	}
+}
