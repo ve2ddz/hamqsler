d431f971ec7201a0944f91ff94a1027fe8e70cfa
Jim Orcheson <jimorcheson@gmail.com>
2013-09-27 08:00:17 -0400

    Added AdifFieldParts and AdifFieldPartsTests classes.

---
+++ HamqslerTest/AdifFieldPartsTests.cs
@@ -0,0 +1,109 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// test class for AdifFieldParts
+	[TestFixture]
+	public class AdifFieldPartsTests
+	{
+		// test Constructor valid string no DataType
+		[Test]
+		public void TestConstructorValidNoDataType()
+		{
+			AdifFieldParts parts = new AdifFieldParts("<Time:6>123456");
+			Assert.AreEqual("Time", parts.Field);
+			Assert.AreEqual("", parts.DataType);
+			Assert.AreEqual("123456", parts.Value);
+			Assert.AreEqual(null, parts.Enumeration);
+		}
+		
+		// test Constructor valid string with whitespace before and after
+		[Test]
+		public void TestConstructorValidWhitespace()
+		{
+			AdifFieldParts parts = new AdifFieldParts(" <Time:6>123456 ");
+			Assert.AreEqual("Time", parts.Field);
+			Assert.AreEqual("", parts.DataType);
+			Assert.AreEqual("123456", parts.Value);
+			Assert.AreEqual(null, parts.Enumeration);
+		}
+		
+		// test Constructor valid string with enumeration
+		[Test]
+		public void TestConstructorValidEnumeration()
+		{
+			AdifFieldParts parts = new AdifFieldParts("<Userdef1:19:E>SweaterSize,{S,M,L}");
+			Assert.AreEqual("Userdef1", parts.Field);
+			Assert.AreEqual("E", parts.DataType);
+			Assert.AreEqual("SweaterSize", parts.Value);
+			string err = string.Empty;
+			Assert.IsTrue(parts.Enumeration.IsInEnumeration("S", out err));
+			Assert.IsTrue(parts.Enumeration.IsInEnumeration("M", out err));
+			Assert.IsTrue(parts.Enumeration.IsInEnumeration("L", out err));
+			Assert.IsFalse(parts.Enumeration.IsInEnumeration("XL", out err));
+		}
+		
+		// test Constructor valid string with range
+		[Test]
+		public void TestConstructorValidRange()
+		{
+			AdifFieldParts parts = new AdifFieldParts("<USERDEF3:8:N>ShoeSize,{5:20}");
+			Assert.AreEqual("USERDEF3", parts.Field);
+			Assert.AreEqual("N", parts.DataType);
+			Assert.AreEqual("ShoeSize", parts.Value);
+			string err = string.Empty;
+			Assert.AreEqual("5", parts.LowerValue);
+			Assert.AreEqual("20", parts.UpperValue);
+		}
+
+		// test Constructor valid string with float range
+		[Test]
+		public void TestConstructorValidRange2()
+		{
+			AdifFieldParts parts = new AdifFieldParts("<USERDEF3:8:N>ShoeSize,{5.2:20.}");
+			Assert.AreEqual("USERDEF3", parts.Field);
+			Assert.AreEqual("N", parts.DataType);
+			Assert.AreEqual("ShoeSize", parts.Value);
+			string err = string.Empty;
+			Assert.AreEqual("5.2", parts.LowerValue);
+			Assert.AreEqual("20.", parts.UpperValue);
+		}
+		
+		// test Constructor valid string with range
+		[Test]
+		public void TestConstructorBadRange()
+		{
+			AdifFieldParts parts = new AdifFieldParts("<USERDEF3:8:N>ShoeSize,{20:5}");
+			Assert.AreEqual(null, parts.Field);
+		}
+		
+		// test Constructor valid string with range
+		[Test]
+		public void TestConstructorBadRange2()
+		{
+			AdifFieldParts parts = new AdifFieldParts("<USERDEF3:8:N>ShoeSize,{20:E}");
+			Assert.AreEqual(null, parts.Field);
+		}
+	}
+}
---
+++ hamqsler/AdifFieldParts.cs
@@ -0,0 +1,111 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Text.RegularExpressions;
+
+namespace hamqsler
+{
+	/// <summary>
+	///  AdifFieldParts class - used to break up an AdifField string into its constituent parts
+	///  AdifField string format is <FieldName:Length of value(:data type - optional)>Value(,{optional enumeration or range})
+	/// </summary>
+	public class AdifFieldParts
+	{
+		// ADIF field name - null if AdifField string is invalid in any way
+		private string field = null;
+		public string Field
+		{
+			get {return field;}
+		}
+		
+		
+		private string value = null;
+		public string Value
+		{
+			get {return value;}
+		}
+		
+		private string dataType = null;
+		public string DataType
+		{
+			get {return dataType;}
+		}
+		
+		private EnumerationField enumField;
+		public EnumerationField Enumeration
+		{
+			get {return enumField;}
+		}
+		
+		private string lowerValue;
+		public string LowerValue
+		{
+			get {return lowerValue;}
+		}
+		
+		private string upperValue;
+		public string UpperValue
+		{
+			get {return upperValue;}
+		}
+		
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="field">Adif field string</param>
+		public AdifFieldParts(string adifField)
+		{
+			char[] trimChars = {' '};
+			adifField = adifField.Trim(trimChars);
+			Regex regex = new Regex("^<([A-Za-z0-9_]+):([1-9][0-9]*)(:([A-Z])){0,1}>" +
+			                        "([A-Za-z0-9_]*)(,{(([A-Za-z0-9_,]+)|([0-9]+(\\.[0-9]*)*:[0-9]+(\\.[0-9]*)*))}){0,1}$");
+			Match m = regex.Match(adifField);
+			if(m.Success)
+			{
+				field = m.Groups[1].Value;
+				dataType = m.Groups[4].Value;
+				value = m.Groups[5].Value;
+				int len = Int32.Parse(m.Groups[2].Value);
+				if(len < value.Length)
+				{
+					value = value.Substring(0, len);
+				}
+				string enumers = m.Groups[8].Value;
+				if(!enumers.Equals(string.Empty))
+				{
+					enumField = new EnumerationField(enumers.Split(','));
+				}
+				string rangeString = m.Groups[9].Value;
+				if(!rangeString.Equals(string.Empty))
+				{
+					string[] range = rangeString.Split(':');
+					lowerValue = range[0];
+					upperValue = range[1];
+					float lower = float.Parse(lowerValue);
+					float upper = float.Parse(upperValue);
+					if(lower > upper)
+					{
+						field = null;		// show bad ADIF field
+					}
+				}
+			}
+		}
+	}
+}
