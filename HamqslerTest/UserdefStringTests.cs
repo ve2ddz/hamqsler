6be9631182fed641cccfec086e9795ac5f94a565
Jim Orcheson <jimorcheson@gmail.com>
2013-09-27 14:08:43 -0400

    Added UserdefString and UserdefStringTests classes.

---
+++ HamqslerTest/UserdefStringTests.cs
@@ -0,0 +1,72 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for UserdefString class
+	[TestFixture]
+	public class UserdefStringTests
+	{
+		AdifEnumerations aEnums;
+		Userdef userdef1;
+		// TestFixtureSetup
+		[TestFixtureSetUp]
+		public void TestSepup()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			aEnums = new AdifEnumerations(str);
+			userdef1 = new Userdef("EPC", "S", aEnums);
+		}
+		
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			UserdefString uds = new UserdefString("1234", userdef1);
+			Assert.AreEqual("<EPC:4:S>1234", uds.ToAdifString());
+		}
+		
+		// test Validate with valid input
+		[Test]
+		public void TestValidateValid()
+		{
+			UserdefString uds = new UserdefString("1234", userdef1);
+			string err = string.Empty;
+			Assert.IsTrue(uds.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		// test Validate with null value
+		[Test]
+		public void TestValidateNullValue()
+		{
+			UserdefString uds = new UserdefString(null, userdef1);
+			string err = string.Empty;
+			Assert.IsFalse(uds.Validate(out err));
+			Assert.AreEqual("Value is null", err);
+		}
+	}
+}
---
+++ hamqsler/UserdefString.cs
@@ -0,0 +1,53 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// UserdefString class - user defined adif field class that does not include an enumeration
+	/// or a range.
+	/// </summary>
+	public class UserdefString : StringField
+	{
+		private Userdef userdef;
+		
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="value">field value</param>
+		/// <param name="userdefField">Userdef object that defines this field type</param>
+		public UserdefString(string value, Userdef userdefField)
+			: base(value)
+		{
+			userdef = userdefField;
+		}
+		
+		/// <summary>
+		/// Create ADIF string for this field
+		/// </summary>
+		/// <returns>ADIF string represented by this object</returns>
+		public override string ToAdifString()
+		{
+			return string.Format("<{0}:{1}:{2}>{3}", userdef.UName, Value.Length,
+			                     userdef.DataType.Value, Value);
+		}
+	}
+}
