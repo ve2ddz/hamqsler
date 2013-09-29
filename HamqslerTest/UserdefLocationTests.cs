6dba555ab617647f36e85792910d58a4b7713801
Jim Orcheson <jimorcheson@gmail.com>
2013-09-29 09:50:22 -0400

    Added UserdefLocation and UserdefLocationTests classes.

---
+++ HamqslerTest/UserdefLocationTests.cs
@@ -0,0 +1,73 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for UserdefLocation class
+	[TestFixture]
+	public class UserdefLocationTests
+	{
+		AdifEnumerations aEnums;
+		Userdef userdef1;
+		// TestFixtureSetup
+		[TestFixtureSetUp]
+		public void TestSepup()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			aEnums = new AdifEnumerations(str);
+			userdef1 = new Userdef("MumbaiLatitude", "L", aEnums);
+		}
+		
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			UserdefLocation uds = new UserdefLocation("N027 26.385", userdef1);
+			Assert.AreEqual("<MumbaiLatitude:11:L>N027 26.385", uds.ToAdifString());
+		}
+		
+		// test Validate with valid input
+		[Test]
+		public void TestValidateValid()
+		{
+			UserdefLocation uds = new UserdefLocation("N027 26.385", userdef1);
+			string err = string.Empty;
+			Assert.IsTrue(uds.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		
+		// test Validate with invalid input
+		[Test]
+		public void TestValidateInvalid()
+		{
+			UserdefLocation uds = new UserdefLocation("U027 26.385", userdef1);
+			string err = string.Empty;
+			Assert.IsFalse(uds.Validate(out err));
+			Assert.AreEqual("'U027 26.385' is not a valid location.", err);
+		}
+	}
+}
---
+++ hamqsler/UserdefLocation.cs
@@ -0,0 +1,51 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// UserdefLocation class - user defined adif field that is Location based
+	/// </summary>
+	public class UserdefLocation : Location
+	{
+		private Userdef userdef;
+		
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="value">field value</param>
+		/// <param name="userdefField">Userdef object that defines this field type</param>
+		public UserdefLocation(string loc, Userdef udef) : base(loc)
+		{
+			userdef = udef;
+		}
+		
+		/// <summary>
+		/// Create ADIF string for this field
+		/// </summary>
+		/// <returns>ADIF string represented by this object</returns>
+		public override string ToAdifString()
+		{
+			return string.Format("<{0}:{1}:{2}>{3}", userdef.UName, Value.Length,
+			                     userdef.DataType.Value, Value);
+		}
+	}
+}
