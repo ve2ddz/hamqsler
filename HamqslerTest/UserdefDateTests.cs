7c6a68900e040c2d47021178d9b411f3f8d3364e
Jim Orcheson <jimorcheson@gmail.com>
2013-09-29 07:19:39 -0400

    Added UserdefDate and UserdefDateTests classes.

---
+++ HamqslerTest/UserdefDateTests.cs
@@ -0,0 +1,73 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for UserdefDate class
+	[TestFixture]
+	public class UserdefDateTests
+	{
+		AdifEnumerations aEnums;
+		Userdef userdef1;
+		// TestFixtureSetup
+		[TestFixtureSetUp]
+		public void TestSepup()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			aEnums = new AdifEnumerations(str);
+			userdef1 = new Userdef("Birthdate", "D", aEnums);
+		}
+		
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			UserdefDate uds = new UserdefDate("20000613", userdef1);
+			Assert.AreEqual("<Birthdate:8:D>20000613", uds.ToAdifString());
+		}
+		
+		// test Validate with valid input
+		[Test]
+		public void TestValidateValid()
+		{
+			UserdefDate uds = new UserdefDate("20000613", userdef1);
+			string err = string.Empty;
+			Assert.IsTrue(uds.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		
+		// test Validate with invalid input
+		[Test]
+		public void TestValidateInvalid()
+		{
+			UserdefDate uds = new UserdefDate("2000061F", userdef1);
+			string err = string.Empty;
+			Assert.IsFalse(uds.Validate(out err));
+			Assert.AreEqual("Date must contain number characters only.", err);
+		}
+	}
+}
---
+++ hamqsler/UserdefDate.cs
@@ -0,0 +1,51 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+
+namespace hamqsler
+{
+	/// <summary>
+	/// UserdefDate class - user defined field of type date
+	/// </summary>
+	public class UserdefDate : DateField
+	{
+		private Userdef userdef;
+		
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="value">field value</param>
+		/// <param name="userdefField">Userdef object that defines this field type</param>
+		public UserdefDate(string value, Userdef userdefField) : base(value)
+		{
+			userdef = userdefField;
+		}
+		
+		/// <summary>
+		/// Create ADIF string for this field
+		/// </summary>
+		/// <returns>ADIF string represented by this object</returns>
+		public override string ToAdifString()
+		{
+			return string.Format("<{0}:{1}:{2}>{3}", userdef.UName, Value.Length,
+			                     userdef.DataType.Value, Value);
+		}
+	}
+}
