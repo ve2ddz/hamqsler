814f4603ba9b4a550ce67b2d49d2b94cfef1bafb
Jim Orcheson <jimorcheson@gmail.com>
2013-09-29 07:27:48 -0400

    Added UserdefTime and UserdefTimeTests classes.

---
+++ HamqslerTest/UserdefTimeTests.cs
@@ -0,0 +1,72 @@
+﻿/*
+ *  Author:
+ *       Jim Orcheson <jimorcheson@gmail.com>
+ * 
+ *  Copyright (c) 2013 Jim Orcheson
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using hamqsler;
+
+namespace hamqslerTest
+{
+	// tests for UserdefTime class
+	public class UserdefTimeTests
+	{
+		AdifEnumerations aEnums;
+		Userdef userdef1;
+		// TestFixtureSetup
+		[TestFixtureSetUp]
+		public void TestSepup()
+		{
+			Assembly assembly = Assembly.GetAssembly((new AdifField()).GetType());
+            Stream str = assembly.GetManifestResourceStream("hamqsler.AdifEnumerations.xml");
+			aEnums = new AdifEnumerations(str);
+			userdef1 = new Userdef("QslTime", "T", aEnums);
+		}
+		
+		// test ToAdifString
+		[Test]
+		public void TestToAdifString()
+		{
+			UserdefTime uds = new UserdefTime("1248", userdef1);
+			Assert.AreEqual("<QslTime:4:T>1248", uds.ToAdifString());
+		}
+		
+		// test Validate with valid input
+		[Test]
+		public void TestValidateValid()
+		{
+			UserdefTime uds = new UserdefTime("124819", userdef1);
+			string err = string.Empty;
+			Assert.IsTrue(uds.Validate(out err));
+			Assert.AreEqual(null, err);
+		}
+		
+		
+		// test Validate with invalid input
+		[Test]
+		public void TestValidateInvalid()
+		{
+			UserdefTime uds = new UserdefTime("124875", userdef1);
+			string err = string.Empty;
+			Assert.IsFalse(uds.Validate(out err));
+			Assert.AreEqual("Time must be in HHMM or HHMMSS format.", err);
+		}
+	}
+}
